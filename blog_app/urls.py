44d85f29bb3b1e6a04a2adfd9371786b6ce75a06
Geoffroy Blondel <geoffroy.blondel@gmail.com>
2021-12-11 17:06:59 +0100

    little fixies

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -16,13 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
-from django.conf.urls import url
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.accueil, name='accueil'),
     path('article/<int:id>', views.lire, name='lire'),
     path('videos/', views.videos, name='videos'),
